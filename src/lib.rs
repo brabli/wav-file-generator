bceb61f2468d3f5830957fa5255b989b56a2e323
Brabli <67018167+Brabli@users.noreply.github.com>
2023-01-22 02:12:41 +0000

    Create func to write specific bytes to a file

--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,59 @@
+mod sine;
+
+use sine::SineOscillator;
 use std::{
-    f64::consts::PI,
     fs::File,
-    io::{Seek, SeekFrom, Write},
+    io::{Result, Seek, SeekFrom, Write},
 };
 
-const SAMPLE_RATE: f64 = 44_100.0;
-const BIT_DEPTH: u32 = 16;
-const MAX_AMPLITUDE: u32 = 2_u32.pow(BIT_DEPTH - 1) - 1;
-
-struct SineOscillator {
-    frequency: f64,
-    amplitude: f64,
-    angle: f64,
-    offset: f64,
+fn write_str_bytes(file: &mut File, string: &str) -> Result<()> {
+    let bytes = string.as_bytes();
+    file.write_all(bytes)
+        .expect(format!("{string} to be written to file").as_str());
+    Ok(())
 }
 
-impl SineOscillator {
-    pub fn new(frequency: f64, amplitude: f64) -> Self {
-        SineOscillator {
-            frequency,
-            amplitude,
-            angle: 0.0,
-            offset: 2.0 * PI * frequency / SAMPLE_RATE,
-        }
-    }
+fn write_int_bytes(file: &mut File, int: u32, num_bytes: u8) -> Result<()> {
+    let bytes = int.to_ne_bytes();
+    let bytes_to_write = bytes
+        .get(..num_bytes as usize)
+        .expect(format!("Couldn't convert {int} into {num_bytes} bytes!").as_str());
 
-    fn process(&mut self) -> f64 {
-        let sample = self.amplitude * self.angle.sin();
-        self.angle += self.offset;
-        sample
-    }
+    file.write_all(&bytes_to_write)
+        .expect(format!("{int} to be written to file").as_str());
+    Ok(())
 }
 
-pub fn run() {
+pub fn run() -> Result<()> {
     let mut osc = SineOscillator::new(440.0, 0.5);
     let mut audio_file = File::create("audio.wav").unwrap();
 
-    audio_file
-        .write("RIFF".as_bytes())
-        .expect("Riff to be written");
+    write_str_bytes(&mut audio_file, "RIFF")?;
+    write_str_bytes(&mut audio_file, "----")?;
+    write_str_bytes(&mut audio_file, "WAVE")?;
+    write_str_bytes(&mut audio_file, "fmt ")?;
 
-    audio_file
-        .write("----".as_bytes())
-        .expect("Size to be written");
+    write_int_bytes(&mut audio_file, 16, 4)?; // fmt length
+    write_int_bytes(&mut audio_file, 1, 2)?; // Compression code
+    write_int_bytes(&mut audio_file, 1, 2)?; // Number of channels
+    write_int_bytes(&mut audio_file, 44_100, 4)?; // Sample rate
 
-    audio_file
-        .write("WAVE".as_bytes())
-        .expect("Wave to be written");
+    let bitrate = (sine::SAMPLE_RATE as u32 * sine::BIT_DEPTH / 8) as u32; // 88_200
+    write_int_bytes(&mut audio_file, bitrate, 4)?; // Bitrate
 
-    audio_file
-        .write("fmt ".as_bytes())
-        .expect("Fmt to be written");
+    write_int_bytes(&mut audio_file, 2, 2)?; // Block align (bitdepth / 8 in this case)
 
-    audio_file
-        .write(&16_u32.to_ne_bytes())
-        .expect("Length of fmt to be written");
+    write_int_bytes(&mut audio_file, sine::BIT_DEPTH, 2)?; // Bit depth (significant bytes)
 
-    audio_file
-        .write(&1_u16.to_ne_bytes())
-        .expect("Compression code to be written");
-
-    audio_file
-        .write(&1_u16.to_ne_bytes())
-        .expect("Num channels rate to be written");
-
-    audio_file
-        .write(&44_100_u32.to_ne_bytes())
-        .expect("Sample rate to be written");
-
-    audio_file
-        .write(&((SAMPLE_RATE as u32 * BIT_DEPTH / 8) as u32).to_ne_bytes())
-        .expect("Bitrate to be written");
-
-    audio_file
-        .write(&2_u16.to_ne_bytes())
-        .expect("Block align to be written (In this case can also be bitdepth / 8)");
-
-    audio_file
-        .write(&(BIT_DEPTH as u16).to_ne_bytes())
-        .expect("Bit depth (sig bytes) to be written");
-
-    audio_file
-        .write("data".as_bytes())
-        .expect("Data header to be written");
-
-    audio_file
-        .write("----".as_bytes())
-        .expect("Data size to be written");
+    write_str_bytes(&mut audio_file, "data")?;
+    write_str_bytes(&mut audio_file, "----")?;
 
     let pre_audio_pos = audio_file.stream_position().unwrap();
     println!("Pre audio pos: {pre_audio_pos}");
 
-    for _ in 0..(SAMPLE_RATE * 2.0) as u32 {
+    for _ in 0..(sine::SAMPLE_RATE * 2.0) as u32 {
         let sample = osc.process();
-        let int_sample: i16 = (sample * MAX_AMPLITUDE as f64) as i16;
+        let int_sample: i16 = (sample * sine::MAX_AMPLITUDE as f64) as i16;
         audio_file.write_all(&int_sample.to_ne_bytes()).unwrap();
     }
 
@@ -114,4 +74,6 @@ pub fn run() {
         .unwrap();
 
     println!("Current pos: {}", audio_file.stream_position().unwrap());
+
+    Ok(())
 }
