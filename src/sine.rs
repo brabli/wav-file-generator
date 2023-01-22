0811a05b594f73b5541bcc51b5837446eeca82e5
Brabli <67018167+Brabli@users.noreply.github.com>
2023-01-22 02:12:17 +0000

    Create sine module

---
+++ src/sine.rs
@@ -0,0 +1,29 @@
+use std::f64::consts::PI;
+
+pub const SAMPLE_RATE: f64 = 44_100.0;
+pub const BIT_DEPTH: u32 = 16;
+pub const MAX_AMPLITUDE: u32 = 2_u32.pow(BIT_DEPTH - 1) - 1;
+
+pub struct SineOscillator {
+    frequency: f64,
+    amplitude: f64,
+    angle: f64,
+    offset: f64,
+}
+
+impl SineOscillator {
+    pub fn new(frequency: f64, amplitude: f64) -> Self {
+        SineOscillator {
+            frequency,
+            amplitude,
+            angle: 0.0,
+            offset: 2.0 * PI * frequency / SAMPLE_RATE,
+        }
+    }
+
+    pub fn process(&mut self) -> f64 {
+        let sample = self.amplitude * self.angle.sin();
+        self.angle += self.offset;
+        sample
+    }
+}
